747b5dc6400faa2ab1ed5f4e051b8f63ee20826b
Saban Ünlü [netTrek] <us@netTrek.de>
2024-01-09 15:25:06 +0100

    setup with first e2e test that works

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,8 @@
-describe('My First Test', () => {
+describe('Testing app component', () => {
   it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
+    cy.visit('/');
+    cy.get('h1').contains('Hello, proleit');
+    cy.contains('Congratulations! Your app is running. 🎉')
+    cy.get('[href="https://angular.dev/tools/devtools"] > span').click()
   })
 })
