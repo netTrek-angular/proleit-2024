522612e972f7a5ba2e8abaea724b85002735644a
Saban Ünlü [netTrek] <us@netTrek.de>
2024-01-10 14:58:49 +0100

    bindings countdown

--- src/app/samples/countdown/countdown.component.ts
+++ src/app/samples/countdown/countdown.component.ts
@@ -8,8 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './countdown.component.scss'
 })
 export class CountdownComponent {
+
+  static readonly MODE_PROGRESS = 'progress';
+  static readonly MODE_COUNTDOWN = 'countdown';
+
+
   percent = 0;
   private timer: number | null = null;
+  private mode: 'progress' | 'countdown'  = CountdownComponent.MODE_PROGRESS;
 
   constructor() {
     this.startTimer();
@@ -19,9 +25,13 @@ export class CountdownComponent {
     if ( this.timer ) {
       this.stopTimer()
     }
-    this.percent = 0;
+    this.percent = this.mode === CountdownComponent.MODE_PROGRESS ? 0 : 100;
     this.timer = window.setInterval(() => {
-      this.incremtent();
+      if ( this.mode === CountdownComponent.MODE_PROGRESS ) {
+        this.incremtent();
+      } else {
+        this.decremtent();
+      }
     }, 100 )
   }
 
@@ -36,6 +46,13 @@ export class CountdownComponent {
   private incremtent( step: number = 10 ) {
     this.percent = Math.min( this.percent + step, 100 );
     if ( this.percent === 100 ) {
+      this.stopTimer();
+    }
+  }
+
+  private decremtent(step: number = 10) {
+    this.percent = Math.max( this.percent - step, 0 );
+    if ( this.percent === 0 ) {
       this.stopTimer();
     }
   }
